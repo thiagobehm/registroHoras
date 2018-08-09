cbd6c435cb68c89ddf45e2e585ec5e7eff1cbc26
Thiago Luis Vasconcellos Behm <thiagoluisbehm@gmail.com>
2018-08-09 16:17:32 -0300

    Updated to show on UI Data

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,31 +1,7 @@
-$(document).on('click', '.submit', function() {
-
-    // var data = $('#myForm ').serialize();
-    
-    // $.ajax('https://jira.e-core.com/login.jsp', {
-    //   type: 'POST',
-    //   accept: "*/*",
-    //   data: data,
-    //   success: function(data) {
-    //     alert('sucesso');
-    //   },
-    //   error: function() {
-    //     console.log('error');
-    //   }
-    // });
-
-    $.ajax('https://jira.e-core.com/rest/ponto/1/batida/byCurrentUser?startDate=06/08/2018&endDate=12/08/2018', {
-      type: 'GET',
-      contentType: "application/json;charset=UTF-8",
-      success: function(data) {
-        alert('sucesso');
-        console.log(data);
-      },
-      error: function() {
-        console.log('error');
-      }
-    });
-    
-  });
+$(document).ready( function() {
+	$(".datepicker").datepicker({
+		dateFormat: 'dd/mm/yy'
+	});
+});
 
 
--- server.js
+++ server.js
@@ -3,32 +3,87 @@ const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
+const request = require('request');
+const base64 = require('base-64');
 
-//configures a variable because of heroku
+
+
+//configures a variable to heroku environment
 const port = process.env.PORT || 3000;
 
-var app = express();
+//starts express
+let app = express();
 
-var urlencodedParser = bodyParser.urlencoded({
+//parses the body parameters
+let urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
 
-
+//register the directory templates of Handlebars
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
+//defines the 'root' directory for public files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//configures the session
 app.use(session({
   secret: 'max',
   saveUninitialized: true,
   resave: false
 }));
 
+
+/*** Receives the user login --- */
+app.post('/login', urlencodedParser, (req, resp) => {
+	let username = req.body.username;
+	let password = req.body.password; 
+	let authentication =  base64.encode(`${username}:${password}`);
+	let startDate = req.body.startDate;
+	let endDate = req.body.endDate;
+
+	
+	request.get( {
+	    url: `https://jira.e-core.com/rest/ponto/1/batida/byCurrentUser?startDate=${startDate}&endDate=${endDate}`,
+	    headers: {
+    		'authorization': `Basic ${authentication}`
+  		},
+		json: true,
+	}, (err, res, body) => {
+	  // if result it's ok		
+	  if (res.statusCode == 200) {
+	  	console.log(body.results);	
+	  	resp.render('index.hbs', {
+	  		results: body.results,
+	  		data: {
+	        	username,
+	        	startDate,
+	        	endDate
+	        }
+	  	});
+	  } else if (res.statusCode == 401){ //invalid credentials
+		  	resp.render('index.hbs', {
+	        error: "Invalid credentials! Please, confirm your user and password",
+	        // provides the data so the user it does not have to input everything again
+	        data: {
+	        	username,
+	        	startDate,
+	        	endDate
+	        }
+      	});
+	  }
+	  
+	});
+	
+	
+});
+
+//renders the index page.
 app.get('/', (req, res) =>{
 	res.render('index.hbs');
 });
 
+//start the server
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
